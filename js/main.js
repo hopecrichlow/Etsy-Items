973d19d3b3426359ff6080a49f5428d66e1efbb6
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-10-01 15:11:58 -0400

    JS for first question

--- js/main.js
+++ js/main.js
@@ -2,4 +2,47 @@
 (function () {
 
 
-})();
+
+//Create an array of just the prices
+
+  // items.forEach( function(item) {
+  //   console.log(item.price); //price of each item
+  // })
+var prices = items.map( function (item) {
+  return item.price;  //+ ' is for ' + item.title ;
+})
+console.log(prices);
+
+
+//Add all of the prices together
+var sum = prices.reduce( function (prev, next){
+  return prev + next;
+})
+console.log(sum);
+
+//Divide by total number of items
+var avg = sum / prices.length;
+console.log(avg);
+
+//Convert it to 2 decimal places
+var converted = avg.toFixed(2);
+console.log(converted);
+
+//Make it a string
+var str = 'The average price is $' + converted;
+console.log(str);
+
+//Make it show up on page
+
+  //Find our answer element
+var answer1 = document.querySelector('#answer1');
+console.log(answer1);
+
+  //Create a node from our above answer ready for the DOM
+var textNode = document.createTextNode(str);
+
+  //Append newly created node to our answer element
+answer1.appendChild(textNode);
+
+
+} ());
